5d90f8ecfa14ea5713c3e550c97dfd76a6dadf2a
dwane richards <dwanerich@gmail.com>
2019-10-31 21:58:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-  p array.include?(element)
+ array.include?("element")
 end
 
 def using_sort(array)
-  p array.using_sort.sort
+ 
 end
 
 def using_reverse(array)
-  p array.using_reverse.reverse
+
 end
 
 def using_first(array)
-   p array.using_first.first
+
 end
 
 def using_last(array)
-  p array.using_last.last
+ 
 end
 
 def using_size(array)
-  p array.using_size.size
+
 end
