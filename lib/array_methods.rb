606a09b74a953036e12f5b9974286d7695c25da3
dwane richards <dwanerich@gmail.com>
2019-10-31 21:39:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+  p using_include.include?('dwane')
 end
 
 def using_sort(array)
-
+  p using_sort.sort
 end
 
 def using_reverse(array)
-
+  p using_reverse.reverse
 end
 
 def using_first(array)
-
+   p using_first.first
 end
 
 def using_last(array)
-
+    p using_last.last
 end
 
 def using_size(array)
-
+    p using_size.size
 end
